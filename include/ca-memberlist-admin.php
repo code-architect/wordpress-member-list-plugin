7d18f2d4c40dbe5497350773bb333d192b624574
code-architect <codearchitectdeveloper@gmail.com>
2016-11-26 22:55:50 +0530

    showing data from the database to the admin area.

--- include/ca-memberlist-admin.php
+++ include/ca-memberlist-admin.php
@@ -8,7 +8,36 @@ add_action('admin_menu', 'ca_memberlist_admin_menu');
 
 function ca_memberlist_admin_menu()
 {
-    add_menu_page('Member-List', 'CA Member-List', 'manage_options', 'memberlist_list', 'memberlist_list', 'dashicons-groups');
+    add_menu_page(
+        'Member-List',      // page title
+        'CA Member-List',   // menu title
+        'manage_options',   // capabilities
+        'memberlist_list',  // menu slug
+        'memberlist_list',  // function
+        'dashicons-groups'  // icon
+    );
 
-    add_submenu_page( 'memberlist_list', 'Add New Member', 'Add New', 'manage_options', 'memberlist_insert' , 'memberlist_insert');
+    add_submenu_page(
+        'memberlist_list',      // parent slug
+        'Add New Member',       // page title
+        'Add New',              // menu title
+        'manage_options',       //capabilities
+        'memberlist_insert' ,   // menu slug
+        'memberlist_insert'     // function
+    );
+}
+
+/**********************************************************
+ * Display Member list action page                        *
+ *********************************************************/
+function memberlist_list()
+{
+    global $wpdb;
+
+    if(!current_user_can('manage_options'))
+    {
+        wp_die('You do not have sufficient permission');
+    }
+
+    include CA_MEMBER_LIST_PLUGIN_DIR.'/pages/ca-memberlist.php';
 }
---
+++ pages/ca-memberlist.php
@@ -0,0 +1,65 @@
+<?php
+/**********************************************************************************
+ * Name:    ca-memberlist.php
+ *
+ * Usage:   Display the member-list in a table
+ **********************************************************************************/
+
+global $wpdb;
+$valid = true;
+$sql = "SELECT * FROM ".$wpdb->prefix."memberlist";
+$formData = $wpdb->get_results($sql);
+if(!$formData)
+{
+    $valid = false;
+    echo '<p><h1>This form is invalid. No Data Found.</h1></p>';
+}
+?>
+<!-- Bootstrap -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+
+    <div class="row">
+        <div class="col-md-9">
+            <h1>Memberlist Admin</h1>
+
+            <form action="" method="post">
+                <input type="hidden" name="listaction" value="insert"/>
+                <button type="submit" class="btn btn-primary">Add New Member</button><br><br>
+            </form>
+
+            <table class="table">
+                <tr class="info">
+                    <th>Name</th>
+                    <th>Phone</th>
+                    <th>Email</th>
+                    <th>Extra</th>
+                    <th>Action</th>
+                </tr>
+                <?php if($valid){
+                foreach($wpdb->get_results($sql) as $key => $row){
+                ?>
+
+                <form action="" method="post">
+                    <tr>
+                        <input type="hidden" name="listaction" value="edit"/>
+                        <input type="hidden" name="memberid" value="<?php echo $row->ca_id; ?>"/>
+                        <th><?php echo $row->ca_name; ?></th>
+                        <th><?php echo $row->ca_phone; ?></th>
+                        <th><?php echo $row->ca_email; ?></th>
+                        <th><?php echo $row->ca_extra; ?></th>
+                        <td>
+                            <div class="btn-group" role="group">
+                                <button type="submit" class="btn btn-primary">
+                                    <span class="glyphicon glyphicon-pencil"></span>
+                                </button>
+                            </div>
+                        </td>
+                    </tr>
+                </form>
+                <?php }
+                }
+                ?>
+            </table>
+        </div>
+    </div>
